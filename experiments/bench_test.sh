7e73ff8a8e83085a5d257777106cb7f4d2c57245
Nikita Wootten <nikita.wootten@gmail.com>
2020-05-03 16:03:56 -0400

    Adjusted scripts to not take as long

--- experiments/bench_test.sh
+++ experiments/bench_test.sh
@@ -2,58 +2,30 @@
 # replace 0.0.0.0 with IP address server is on
 
 #$1 = address, $2 = output file prefix, $3 = image file name
-
-ADDR1=$1
-ADDR1+="/hello_world"
 PREFIX=$2
+ADDR=$1
+
+FIB="$ADDR/fib"
+HELLO="$ADDR/hello_world"
+RESIZE="$ADDR/resize"
+
 echo "Stress tests for Load-balancing"
 echo "Test hello_world endpoint"
-ab -g ${PREFIX}-hello1.tsv -n 10000 -c 10 "$ADDR1"
+ab -g ${PREFIX}-hello3.tsv -n 100000 -c 100 "$HELLO"
 sleep 10
-ab -g ${PREFIX}-hello2.tsv -n 50000 -c 50 "$ADDR1"
-sleep 10
-ab -g ${PREFIX}-hello3.tsv -n 100000 -c 100 "$ADDR1"
-sleep 10
-wrk -t 4 -c100 -d30s "http://$ADDR1" >> ${PREFIX}-hello1.txt
-sleep 10
-wrk -t 4 -c100 -d30s "http://$ADDR1" >> ${PREFIX}-hello2.txt
-sleep 10
-wrk -t 4 -c100 -d30s "http://$ADDR1" >> ${PREFIX}-hello3.txt
+echo "Test hello_world endpoint wrk"
+wrk -t 4 -c100 -d30s "http://$HELLO" >> ${PREFIX}-hello.txt
 sleep 10
 
-
-ADDR2=$1
-ADDR2+="/fib"
 echo "Test fibonacci endpoint" 
-ab -g ${PREFIX}-fib1.tsv -n 10000 -c 10 "$ADDR2"
+ab -g ${PREFIX}-fib3.tsv -n 100000 -c 100 "$FIB"
 sleep 10
-ab -g ${PREFIX}-fib2.tsv -n 50000 -c 50 "$ADDR2"
-sleep 10
-ab -g ${PREFIX}-fib3.tsv -n 100000 -c 100 "$ADDR2"
-sleep 10
-wrk -t 4 -c100 -d30s "http://$ADDR2" >> ${PREFIX}-fib1.txt
-sleep 10
-wrk -t 4 -c100 -d30s "http://$ADDR2" >> ${PREFIX}-fib2.txt
-sleep 10
-wrk -t 4 -c100 -d30s "http://$ADDR2" >> ${PREFIX}-fib3.txt
+echo "Test fibonacci endpoint wrk"
+wrk -t 4 -c100 -d30s "http://$FIB" >> ${PREFIX}-fib3.txt
 sleep 10
 
-
-ADDR3=$1
-ADDR3+="/resize"
 echo "Test resize endpoint" 
-
-ab -g ${PREFIX}-resize1.tsv -n 1000 -c 10 -p $3 -T image/jpeg "$ADDR3"
+ab -g ${PREFIX}-resize3.tsv -n 10000 -c 100 -p $3 -T image/jpeg "$RESIZE"
 sleep 10
-ab -g ${PREFIX}-resize2.tsv -n 5000 -c 50 -p $3 -T image/jpeg "$ADDR3"
-sleep 10
-ab -g ${PREFIX}-resize3.tsv -n 10000 -c 100 -p $3 -T image/jpeg "$ADDR3" 
-sleep 10
-wrk -s postimg.lua -t 4 -c 5 -d 10s "http://$ADDR3" >> ${PREFIX}-resize1.txt
-sleep 10
-wrk -s postimg.lua -t 4 -c 10 -d 10s "http://$ADDR3" >> ${PREFIX}-resize2.txt
-sleep 10
-wrk -s postimg.lua -t 4 -c 20 -d 10s "http://$ADDR3" >> ${PREFIX}-resize3.txt
-
-
-
+echo "Test resize endpoint wrk"
+wrk -s postimg.lua -t 4 -c 20 -d 20s "http://$RESIZE" >> ${PREFIX}-resize3.txt
