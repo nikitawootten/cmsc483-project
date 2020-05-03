87009488e3a7b491bcf7b22f47c08c94369bba4b
Nikita Wootten <nikita.wootten@gmail.com>
2020-05-02 22:48:35 -0400

    Adjusted bench test to more reasonable values

--- experiments/bench_test.sh
+++ experiments/bench_test.sh
@@ -8,34 +8,34 @@ ADDR1+="/hello_world"
 PREFIX=$2
 echo "Stress tests for Load-balancing"
 echo "Test hello_world endpoint"
-ab -g ${PREFIX}-hello1.tsv -n 10000 -c 200 "$ADDR1"
+ab -g ${PREFIX}-hello1.tsv -n 10000 -c 10 "$ADDR1"
 sleep 10
-ab -g ${PREFIX}-hello2.tsv -n 50000 -c 200 "$ADDR1" 
+ab -g ${PREFIX}-hello2.tsv -n 50000 -c 50 "$ADDR1"
 sleep 10
-ab -g ${PREFIX}-hello3.tsv -n 100000 -c 200 "$ADDR1"
+ab -g ${PREFIX}-hello3.tsv -n 100000 -c 100 "$ADDR1"
 sleep 10
-wrk -t 4 -c101 -d30s "$ADDR1" >> ${PREFIX}-hello1.txt
+wrk -t 4 -c100 -d30s "$ADDR1" >> ${PREFIX}-hello1.txt
 sleep 10
-wrk -t 4 -c202 -d30s "$ADDR1" >> ${PREFIX}-hello2.txt
+wrk -t 4 -c100 -d30s "$ADDR1" >> ${PREFIX}-hello2.txt
 sleep 10
-wrk -t 4 -c303 -d30s "$ADDR1" >> ${PREFIX}-hello3.txt
+wrk -t 4 -c100 -d30s "$ADDR1" >> ${PREFIX}-hello3.txt
 sleep 10
 
 
 ADDR2=$1
 ADDR2+="/fib"
 echo "Test fibonacci endpoint" 
-ab -g ${PREFIX}-fib1.tsv -n 10000 -c 200 "$ADDR2"  
+ab -g ${PREFIX}-fib1.tsv -n 10000 -c 10 "$ADDR2"
 sleep 10
-ab -g ${PREFIX}-fib2.tsv -n 50000 -c 200 "$ADDR2" 
+ab -g ${PREFIX}-fib2.tsv -n 50000 -c 50 "$ADDR2"
 sleep 10
-ab -g ${PREFIX}-fib3.tsv -n 100000 -c 200 "$ADDR2" 
+ab -g ${PREFIX}-fib3.tsv -n 100000 -c 100 "$ADDR2"
 sleep 10
-wrk -t 4 -c101 -d30s "$ADDR2" >> ${PREFIX}-fib1.txt
+wrk -t 4 -c100 -d30s "$ADDR2" >> ${PREFIX}-fib1.txt
 sleep 10
-wrk -t 4 -c202 -d30s "$ADDR2" >> ${PREFIX}-fib2.txt
+wrk -t 4 -c100 -d30s "$ADDR2" >> ${PREFIX}-fib2.txt
 sleep 10
-wrk -t 4 -c303 -d30s "$ADDR2" >> ${PREFIX}-fib3.txt
+wrk -t 4 -c100 -d30s "$ADDR2" >> ${PREFIX}-fib3.txt
 sleep 10
 
 
@@ -43,9 +43,9 @@ ADDR3=$1
 ADDR3+="/resize"
 echo "Test resize endpoint" 
 
-ab -g ${PREFIX}-resize1.tsv -n 1000 -c 100 -p $3 -T image/jpeg "$ADDR3" 
+ab -g ${PREFIX}-resize1.tsv -n 1000 -c 10 -p $3 -T image/jpeg "$ADDR3"
 sleep 10
-ab -g ${PREFIX}-resize2.tsv -n 5000 -c 100 -p $3 -T image/jpeg "$ADDR3" 
+ab -g ${PREFIX}-resize2.tsv -n 5000 -c 50 -p $3 -T image/jpeg "$ADDR3"
 sleep 10
 ab -g ${PREFIX}-resize3.tsv -n 10000 -c 100 -p $3 -T image/jpeg "$ADDR3" 
 sleep 10
